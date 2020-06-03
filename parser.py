922af33487e8e7750dc5c43bd72806b690e39023
Nikolay Sheregeda <ns.sheregeda@gmail.com>
2020-06-03 21:38:17 +0300

    [master] Добавлены тесты парсера категории сайта

---
+++ parser.py
@@ -0,0 +1,164 @@
+import csv
+import os
+
+from collections import Counter
+
+import nltk
+
+from requests_html import HTMLSession
+from pymystem3 import Mystem
+
+
+try:
+    nltk.corpus.stopwords.words('russian')
+except LookupError:
+    nltk.download('stopwords')
+
+
+SITE_URL = 'https://lenta.ru'
+CSS_CATEGORY_ITEM = '.b-sidebar-menu__list-item'
+CSS_NEWS_ITEM = '.row .js-content'
+CSS_NEWS_TEXT = '.b-text p'
+
+
+class LentaRuParserCategory:
+    """
+    Инкапсулирует логику парсинга текста новостей из определенной категории
+    сайта lenta.ru, а также подсчет частотности встречаемых в новостях слов.
+    """
+    def __init__(self, url, session, count_news):
+        self.url = url
+        self.session = session
+        self.count_news = count_news
+        self.stem = Mystem()
+        self.stop_words = set(nltk.corpus.stopwords.words('russian'))
+
+    def extract_news_urls(self, category_url, selector=CSS_NEWS_ITEM):
+        """
+        Извлечь абсолютные ссылки на страницы новостей из определенной
+        категории сайта. Возвращает список статей количеством не больше, чем
+        значение атрибута `count_news`.
+
+        :param category_url:
+            `str`, абсолютная ссылка на страницу со статьями новостей.
+        :param selector:
+            `str`, css селектор для поиска статей на странице сайта.
+
+        :return:
+            `list`, список абсолютных ссылок.
+        """
+        response = self.session.get(category_url)
+        el = response.html.find(selector)
+        if not el:
+            return []
+        news_urls = [i for i in sorted(el[0].absolute_links) if '/news/' in i]
+        return news_urls[:self.count_news]
+
+    def extract_text_news(self, news_url, selector=CSS_NEWS_TEXT):
+        """
+        Извлечь текст статьи по ссылке.
+
+        :param news_url:
+            `str`, абсолютная ссылка на страницу новости.
+        :param selector:
+            `str`, css селектор для извлечения текста статьи на странице.
+
+        :return:
+            `str`, текст статьи сайта.
+        """
+        resp = self.session.get(news_url)
+        return ' '.join([i.text for i in resp.html.find(selector)])
+
+    def preprocess_text(self, text_news):
+        """
+        Выполняет прероцессинг текста статьи:
+          - приводит все слова к нижнему регистру;
+          - выполняет лемматизацию теста;
+          - отбрасывает стоп-слова и пунктуацию;
+          - выбирает слова, которые содержат хотя бы один буквенный символ.
+
+        :param text_news:
+            `str`, текст статьи.
+
+        :return:
+            `list`, список обработанных и отфильтрованных слов статьи.
+        """
+        return [
+            word for word in self.stem.lemmatize(text_news.lower())
+            if word.isalpha() and word not in self.stop_words
+        ]
+
+    def get_words(self):
+        """
+        Посчитать количество слов, которые встречаются в текстах новостных
+        статьей.
+
+        :return:
+            `collections.Counter`, счетчик слов.
+        """
+        counters = []
+        for news_url in self.extract_news_urls(self.url):
+            text_news = self.extract_text_news(news_url)
+            counters.append(Counter(self.preprocess_text(text_news)))
+        return sum(counters, Counter())
+
+
+class LentaRuParserTopWords:
+    def __init__(self, count_words, data_dir, count_news, url=SITE_URL):
+        self.url = url
+        self.session = HTMLSession()
+        self.count_words = count_words
+        self.data_dir = data_dir
+        self.count_news = count_news
+
+    def extract_categories(self, selector=CSS_CATEGORY_ITEM):
+        resp = self.session.get(self.url)
+        categories = {}
+        for el in resp.html.find(selector):
+            if not el.absolute_links:
+                continue
+            link = el.absolute_links.pop()
+            if '/rubrics/' in link:
+                name = link.rstrip('/').rsplit('/', 1)[-1]
+                categories[name] = link
+        return categories
+
+    def save_csv(self, category_name, counter):
+        csv_name = os.path.join(self.data_dir, f'{category_name}.csv')
+        with open(csv_name, 'w') as f:
+            writer = csv.DictWriter(f, fieldnames=('word', 'frequency'))
+            writer.writeheader()
+            for word, frequency in counter.most_common(self.count_words):
+                writer.writerow({'word': word, 'frequency': frequency})
+
+    def exclude_counters_intersection(self, counters):
+        max_counter = Counter()
+        for counter in counters.values():
+            max_counter = max_counter | counter
+
+        result = {}
+        already_use = set()
+        for name, counter in counters.items():
+            new_counter = Counter()
+            for word, count in counter.items():
+                if count == max_counter[word] and word not in already_use:
+                    new_counter[word] = count
+                    already_use.add(word)
+            result[name] = new_counter
+
+        return result
+
+    def run(self):
+        counters = {}
+        for name, category_url in self.extract_categories().items():
+            parser = LentaRuParserCategory(
+                url=category_url,
+                session=self.session,
+                count_news=self.count_news,
+            )
+            counters[name] = parser.get_words()
+
+        counters = self.exclude_counters_intersection(counters)
+
+        for name, counter in counters.items():
+            self.save_csv(name, counter)
---
+++ tests/test_category_parser.py
@@ -0,0 +1,131 @@
+from collections import Counter
+from urllib.parse import urljoin
+
+import pytest
+
+from requests_html import HTMLSession
+
+from parser import (
+    LentaRuParserCategory, CSS_NEWS_ITEM, CSS_NEWS_TEXT, SITE_URL
+)
+
+
+@pytest.fixture()
+def css_news_item():
+    return CSS_NEWS_ITEM.replace('.', '')
+
+
+@pytest.fixture()
+def css_news_text():
+    return CSS_NEWS_TEXT.replace('.', '')
+
+
+@pytest.fixture()
+def category_url():
+    return 'https://lenta.ru/rubrics/world/'
+
+
+@pytest.fixture()
+def news_url():
+    return 'https://lenta.ru/news/2018/09/15/bestplaces/'
+
+
+@pytest.fixture()
+def session():
+    return HTMLSession()
+
+
+@pytest.fixture()
+def count_news():
+    return 2
+
+
+@pytest.fixture()
+def parser(category_url, session, count_news):
+    return LentaRuParserCategory(category_url, session, count_news)
+
+
+def test_extract_news_urls_ok(
+    requests_mock, parser, css_news_item, category_url, count_news
+):
+    news_urls = ['/news/bar/', '/news/foo/', '/news/baz/']
+    text = f"""
+    <div class="row">
+        <div class="{css_news_item}">
+            <a href="/some/url/"></a>
+            <a href="{news_urls[0]}"></a>
+            <a href="{news_urls[1]}"></a>
+            <a href="{news_urls[2]}"></a>
+        </div>
+    </div>
+    """
+    requests_mock.get(category_url, text=text)
+    result = parser.extract_news_urls(category_url)
+    assert len(result) == count_news
+    assert sorted(result) == sorted(
+        [urljoin(SITE_URL, i) for i in news_urls]
+    )[:count_news]
+
+
+def test_extract_news_urls_not_found(requests_mock, parser, category_url):
+    text = '<div class="row">There is no CSS_NEWS_ITEM</div>'
+    requests_mock.get(category_url, text=text)
+    assert parser.extract_news_urls(category_url) == []
+
+
+def test_extract_text_news_ok(requests_mock, parser, css_news_text, news_url):
+    text = f"""
+    <div class="{css_news_text}">
+        <p>some</p>
+        <p>text</p>
+        <div><span>some block</span></div>
+    </div>
+    """
+    requests_mock.get(news_url, text=text)
+    assert parser.extract_text_news(news_url) == 'some text'
+
+
+def test_extract_text_news_not_found(requests_mock, parser, news_url):
+    text = '<div class="text">There is no CSS_NEWS_TEXT</div>'
+    requests_mock.get(news_url, text=text)
+    assert parser.extract_text_news(news_url) == ''
+
+
+@pytest.mark.parametrize('text,result', [
+    ('РеГистР', ['регистр']),
+    ('451° по Фаренгейту', ['фаренгейт']),
+    ('и на в слово', ['слово']),
+    ('несколько слов в тексте!', ['несколько', 'слово', 'текст']),
+    ('пунктуация: , ! ... — «Лето»', ['пунктуация', 'лето']),
+    ('цифры 146 42 17', ['цифра']),
+])
+def test_preprocess_text(parser, text, result):
+    assert parser.preprocess_text(text) == result
+
+
+def test_get_words(
+    requests_mock, parser, category_url, css_news_item, css_news_text
+):
+    news_urls = ['/news/bar', '/news/baz']
+    category_page = f"""
+    <div class="row">
+        <div class="{css_news_item}">
+            <a href="{news_urls[0]}"></a>
+            <a href="{news_urls[1]}"></a>
+        </div>
+    </div>
+    """
+    requests_mock.get(category_url, text=category_page)
+
+    news_page = f"""
+    <div class="{css_news_text}">
+        <p>one</p>
+        <p>two two</p>
+        <p>three three three</p>
+    </div>
+    """
+    for url in news_urls:
+        requests_mock.get(urljoin(SITE_URL, url), text=news_page)
+
+    result = parser.get_words()
+    assert result == Counter({'one': 2, 'two': 4, 'three': 6})
